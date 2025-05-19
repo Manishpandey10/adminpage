c514f7a19ae62139d7e856b51c120785a8daf84e
Manishpandey10 <manish2310pandey@gmail.com>
2025-05-19 22:37:37 +0530

    Middleware fixes

--- routes/form/formroute.php
+++ routes/form/formroute.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\FormController\BasicFormController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('auth')->group(function(){
+Route::middleware('auth')->group(function(){
     Route::get('/form',[BasicFormController::class, 'viewform'])->name('basicform');
     Route::post('/form',[BasicFormController::class, 'registeruser'])->name('adduser');
 });
